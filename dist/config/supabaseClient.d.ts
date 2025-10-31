f45a459389cff3ad7acce22d19bdec4e6eba9e5d
David-Horjet <davidojetola956@gmail.com>
2025-10-31 05:08:59 +0100

    chore: deleted dist

--- dist/config/supabaseClient.d.ts
+++
@@ -1,3 +0,0 @@
-import { SupabaseClient } from '@supabase/supabase-js';
-export declare const supabase: SupabaseClient;
-//# sourceMappingURL=supabaseClient.d.ts.map
--- dist/jobs/finalizePools.job.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const startPoolFinalizationJob: () => void;
-//# sourceMappingURL=finalizePools.job.d.ts.map
